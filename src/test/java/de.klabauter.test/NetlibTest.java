f8815b9eca469bcf90757df2c36611ff436b89f2
mib <marc@marc-brewer.de>
2018-03-26 11:20:06 +0200

    Started Testimplementations.

---
+++ src/test/java/de.klabauter.test/NetlibTest.java
@@ -0,0 +1,15 @@
+package de.klabauter.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NetlibTest {
+
+
+
+    @Test
+    public void contextLoads() {
+        Assertions.assertTrue(1 == 1);
+    }
+
+}
