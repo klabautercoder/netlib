0d7a8e2bcc8f7d9aac494b3f54ff58dd59d79f03
mib <marc@marc-brewer.de>
2018-03-26 11:23:22 +0200

    Using hoverfly to mock webservice communications

--- src/test/java/de.klabauter.test/NetlibTest.java
+++ src/test/java/de.klabauter.test/NetlibTest.java
@@ -3,6 +3,9 @@ package de.klabauter.test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @see <a href="https://specto.io/blog/2017/1/4/stubbing-http-apis-and-microservices-with-the-hoverfly-java-dsl/">Hoverfly</a>
+ */
 class NetlibTest {
 
 
